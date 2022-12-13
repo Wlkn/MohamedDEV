dd902c90a1a4b6d1f9402cc56f8b030b07ed1a83
Wlkn <alibachar27@gmail.com>
2022-12-13 17:46:07 -0500

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document(): JSX.Element {
 		<Html lang="en">
 			<Head>
 				<link rel="icon" type="image/png" href="../../public/favicon.png" />
+				<meta property="og:title" content="Banner" />
+				<meta property="og:description" content="Banner" />
 				<meta property="og:image" content="../../public/banner.png" />
 			</Head>
 			<body className="antialiased font-inter bg-gray-50 text-gray-500 dark:bg-gray-900 selection:bg-gray-900 selection:dark:bg-white selection:text-white selection:dark:text-primary-500">
